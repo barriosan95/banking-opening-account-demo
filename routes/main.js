808e4ddad15e484d9d13c674259825cc8d7c139b
EC2 Default User <ec2-user@ip-172-31-85-234.ec2.internal>
2018-11-20 23:54:47 +0000

    front-end info-form

--- routes/main.js
+++ routes/main.js
@@ -14,5 +14,13 @@ router.get("/savings",function(req,res){
     res.render('savings');
 });
 
+router.get("/checking/application",function(req,res){
+   res.render('checking_app'); 
+});
+
+router.get("/checking/application/customerInfo",function(req,res){
+   res.render('info_app'); 
+});
+
 
 module.exports = router;
